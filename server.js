d712ccd9548fd776c053c8e44e586b5ff6c3ba31
tiendasolzen <soporte.solzen@outlook.com>
2026-08-05 20:52:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(helmet());
 app.use(express.json({ limit: '100kb' }));
 
 // Solo tu tienda de Shopify puede llamar a esta API.
-const ALLOWED_ORIGINS = ['https://tiendasolzen.myshopify.com'];
+const ALLOWED_ORIGINS = ['https://tiendasolzen.myshopify.com', 'https://luneria-uruguay.myshopify.com'];
 
 app.use(
   cors({

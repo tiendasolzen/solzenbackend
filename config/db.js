196eb780771779b5634d90c3b73359cea014661f
tiendasolzen <soporte.solzen@outlook.com>
2026-08-06 16:27:10 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGODB_URI);
   console.log('MongoDB conectado');
 }
 
